9772d8d7c2173aa4dff9a648a3e5184ef55dfed4
mansi sharma <68988348+MS-VIBES@users.noreply.github.com>
2023-09-13 14:08:03 +0530

    Add files via upload

---
+++ 1094. That is my score!.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    a=[0]*8
+    for j in range(n):
+        x,y=map(int,input().split())
+        if x<=8:
+            a[x-1]=max(a[x-1],y)
+    print(sum(a))
